8d82b12b93aabd84e21e6f1377442abc8dad1bac
GD-Coder <grd8198@gmail.com>
2017-07-04 17:13:28 -0500

    implemented required functionality

--- day-1-assignments/app.js
+++ day-1-assignments/app.js
@@ -1,39 +1,39 @@
 let score = 0
 let stopFlag = 0
 let cookie = localStorage.getItem('score')
-const msg = 'You can\'t use that yet'
+const errorMsg = 'You can\'t use that yet'
 const displayScore = (a) => { return $('#score').html(' ' + a) }
 const add = () => {
   score++
   return score
 }
 const addTimed = () => {
-  if(stopFlag === 0) {
-  displayScore(add())
-  setTimeout(addTimed, 1000)
-} else {
+  if (stopFlag === 0) {
+    displayScore(add())
+    setTimeout(addTimed, 1000)
+  } else {
     stopFlag = 0
   }
 }
 const clearScore = () => {
   localStorage.removeItem('score')
+  stopFlag = 1
   score = 0
   displayScore(score)
-  stopFlag = 1
 }
 const checkScore = () => {
-  if(score > 100) {
-  $('#auto_button').css('display', 'block')
-  $('#mult_button').css('display', 'block')
-} else if (score > 9 && score < 100) {
-  $('#mult_button').css('display', 'block')
-  $('#auto_button').css('display', 'none')
-} else {
-  $('#auto_button').css('display', 'none')
-  $('#mult_button').css('display', 'none')
+  if (score > 100) {
+    $('#auto_button').css('display', 'block')
+    $('#mult_button').css('display', 'block')
+  } else if (score > 9 && score < 100) {
+    $('#mult_button').css('display', 'block')
+    $('#auto_button').css('display', 'none')
+  } else {
+    $('#auto_button').css('display', 'none')
+    $('#mult_button').css('display', 'none')
+  }
 }
-}
-$(document).ready(function () {
+$(document).ready(() => {
   if(cookie !== null) {
     score = cookie
     displayScore(score)
@@ -45,13 +45,13 @@ $(document).ready(function () {
     switch (btn) {
       case 'auto_button':
       score < 100
-      ? alert(msg)
+      ? alert(errorMsg)
       : score = score - 100
         addTimed()
         break
       case 'mult_button':
       score < 10
-      ? alert(msg)
+      ? alert(errorMsg)
       : score = (score * 1.2) - 10
         displayScore(score)
         break
@@ -62,8 +62,7 @@ $(document).ready(function () {
       default:
         displayScore(add())
     }
-  checkScore()
-  localStorage.setItem('score', score)
-  console.log(score)
+    checkScore()
+    localStorage.setItem('score', score)
   })
 })
